24dcd92564a7c895ed3e60f8a1e5307bf0f33b48
emeryjdk <41065085+emeryjdk@users.noreply.github.com>
2019-01-17 10:48:28 -0600

    Update structures.py
    When pasted into a Jupyter notebook (which no I understand you intended to make this a called function, not a stand-alone file) there were some indentation issues that I've corrected.
    
    Here's what I suggest before we merge:
    
    1. Be more explicit about the the roles the different parts of the code serve. Make sure that your comments are focused on helping others understand the code.
    2. Add a preamble in which to describe the overall goals of the script and your approach.
    3. You mentioned that there is another approach to writing the code that will improve its performance. Make those changes.
    4. Include what you think might be limitations to the script - can we achieve all our goals at producing our crystal models in this way? Will have have to take another route?
    5. Explore compatiability with our main script - ipyvolume-testing.ipynb. How do we create a structure library and easily access the structures?

--- structures.py
+++ structures.py
@@ -25,7 +25,7 @@ def FCC(L_x,L_y,L_z):
         for S in range(L_x-1):
             for A in range(L_z):
                 z.append(float(A))
-            
+
     for O in range(L_x-1):
         for S in range((L_y)*(L_z-1)):
             x.append(float(O+.5))
@@ -37,7 +37,7 @@ def FCC(L_x,L_y,L_z):
         for S in range(L_x-1):
             for A in range(L_z-1):
                 z.append(float(A+.5))
-            
+
     for O in range(L_x):
         for S in range((L_y-1)*(L_z-1)):
             x.append(float(O))
@@ -53,7 +53,7 @@ def FCC(L_x,L_y,L_z):
     Y = np.array(y)
     Z = np.array(z)
 
-    #remods atoms ond the back face so that when it is shifted no atoms are out of the crystil
+     #remods atoms ond the back face so that when it is shifted no atoms are out of the crystil
     NX = np.concatenate((X[0:(L_y*L_z*(L_x-1))],X[L_y*L_z*L_x:-((L_y-1)*(L_z-1))]), axis = None)+.5
     NY = np.concatenate((Y[0:(L_y*L_z*(L_x-1))],Y[L_y*L_z*L_x:-((L_y-1)*(L_z-1))]), axis = None)
     NZ = np.concatenate((Z[0:(L_y*L_z*(L_x-1))],Z[L_y*L_z*L_x:-((L_y-1)*(L_z-1))]), axis = None)
@@ -70,7 +70,7 @@ def FCC(L_x,L_y,L_z):
     for O in range(L_y):
         for A in range(L_z-1):
             Nz.append(float(A+.5))
-        
+
     for O in range((L_y-1)*(L_z)):
         Nx.append(0.0)
     for O in range(L_z):
